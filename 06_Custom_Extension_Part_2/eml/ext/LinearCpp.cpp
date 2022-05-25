aa3c5cdb506214f1bea8279a831b7d28e860c6dd
niklas <rausch.nik@gmail.com>
2022-05-25 18:38:29 -0400

    added backward pass

--- 06_Custom_Extension_Part_2/eml/ext/LinearCpp.cpp
+++ 06_Custom_Extension_Part_2/eml/ext/LinearCpp.cpp
@@ -12,8 +12,18 @@ torch::Tensor forward(  torch::Tensor i_inputs,
                                             i_weights);               
     
     return l_result;
+}
 
+std::vector< torch::Tensor > backward( torch::Tensor i_grad_output,
+                                       torch::Tensor i_input,
+                                       torch::Tensor i_weights ){
 
+    std::cout << "Hello World from backward" << std::endl;
+
+    std::vector< torch::Tensor > l_result = torch.matmul( i_grad_output, l_weights.transpose(0, 1) ),
+                                            torch.matmul( l_input.transpose(0, 1), i_grad_output );
+
+    return l_result;
 }
 
 PYBIND11_MODULE( TORCH_EXTENSION_NAME,
