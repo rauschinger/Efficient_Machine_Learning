747fc4950dc5260fb83c9fc610e15ec9dc6377fd
niklas <rausch.nik@gmail.com>
2022-06-10 09:12:24 -0400

    added Conv2dlm2col.cpp from lecture + backup from cpp file

--- 10_convolution_on_steroids_part_1/mini_dnn/src/backend/Conv2dIm2col.cpp
+++ 10_convolution_on_steroids_part_1/mini_dnn/src/backend/Conv2dIm2col.cpp
@@ -11,10 +11,44 @@ at::Tensor mini_dnn::backend::Conv2dIm2col::forward( at::Tensor i_input,
   MINI_DNN_CHECK_EQ( l_sizes.bk, 1 );
 
 
-  // TODO: finish implementation
+  // assign kernel sizes
+  std::vector< int64_t > l_kernel_sizes = { l_sizes.r, 
+                                            l_sizes.s };
 
-  // TODO: remove dummy
-  at::Tensor l_output = at::zeros( {l_sizes.n, l_sizes.kb, l_sizes.p, l_sizes.q} );
+  // assign dilation
+  std::vector< int64_t > l_dilations = { 1, 1 };
+  
+  // assign padding
+  std::vector< int64_t > l_paddings = { 0, 0 };
+  
+  // assign stride
+  std::vector< int64_t > l_strides = { 1,1 };
+  
+  std::cout << "i_input: " << i_input.sizes() << std::endl;
+  
+  // cal im2col
+  at::Tensor l_input = at::im2col( l_input,
+                                   l_kernel_sizes,
+                                   l_dilations,
+                                   l_paddings,
+                                   l_strides );
+                                   
+  std::cout << "l_input: " << l_input.sizes() << std::endl;
+  
+  
+  // multiply tensor with weights
+  std::cout << "i_weight: " << i_weight.szes() << std::endl;
+  
+  at::Tensor l_weight = at::flatten( i_weight, 1 );
+  
+  std::cout << "l_weight: " << l_weight.sizes()  << std::endl;
+  
+  at::Tensor l_output = at::matmul( l_weight,
+                                    l_input );
+  
+  std::cout << "l_output [n, k, p*q]: " << l_output.sizes() << std::endl;
 
+  l_output = l_output.view( { l_sizes.n, l_sizes.kb, l_sizes.p, l_sizes.q } ); 
+  
   return l_output;
 }
